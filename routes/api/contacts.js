8e65b2dfde9f6b7d8a99a334435e1291592cfa5b
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-06-22 21:09:05 +0300

    add putch, isValidId. Done hw

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,27 +6,46 @@ const {
   addContact,
   updateContact,
   removeContact,
+  updateFavorite,
 } = require("../../controllers/contactsController");
 
-const { contactAddSchema } = require("../../schemas/contacts");
+const {
+  contactAddSchema,
+  contactUpdateFavoriteSchema,
+} = require("../../schemas/contacts");
 
-const { validateBody, isBodyInRequest } = require("../../decorators");
+const {
+  validateBody,
+  isBodyInRequest,
+  isBodyFavoriteInRequest,
+} = require("../../decorators");
+
+const { isValidId } = require("../../middlewares");
 
 const router = express.Router();
 
 router.get("/", listContacts);
 
-router.get("/:contactId", getContactById);
+router.get("/:contactId", isValidId, getContactById);
 
 router.post("/", validateBody(contactAddSchema), addContact);
 
-router.delete("/:contactId", removeContact);
+router.delete("/:contactId", isValidId, removeContact);
 
 router.put(
   "/:contactId",
+  isValidId,
   isBodyInRequest,
   validateBody(contactAddSchema),
   updateContact
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  isBodyFavoriteInRequest,
+  validateBody(contactUpdateFavoriteSchema),
+  updateFavorite
 );
 
 module.exports = router;
