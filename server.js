fd38a95708f416641dbe82c0536ac691cfc1697d
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-06-22 13:03:21 +0300

    change name file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const { DB_HOST } = require("./confiq");
+const { DB_HOST } = require("./config");
 
 mongoose
   .connect(DB_HOST)
